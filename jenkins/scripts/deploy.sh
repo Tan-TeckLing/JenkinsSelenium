e51361a04423e650936998a26673746171346fa3
Tan Teck Ling <teckling1998@gmail.com>
2023-11-13 02:19:09 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env -S sh
+#!/usr/bin/env sh
 
-set -x
+# set -x
 docker run -d -p 80:80 --name my-apache-php-app -v /home/Selenium/JenkinsSelenium-main/src:/var/www/html php:7.2-apache
 sleep 1
-set +x
+# set +x
 
 echo 'Now...'
 echo 'Visit http://localhost to see your PHP application in action.'
